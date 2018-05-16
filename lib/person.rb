85e7a165826af60826e6862c915197478e32a252
github username <github email address>
2018-05-15 22:27:24 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
-# your code goes here
+class Person
+
+    #add name to attr_reader so that it cannot be overwritten
+    attr_writer
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def hygiene=(hygiene_score)
+        @hygiene = hygiene_score
+        @hygiene.clamp(0,10)
+    end
+
+    def happiness=(happiness_score)
+        @happiness = happiness_score
+        @happiness.clamp(0,10)
+
+    end
+
+
+
+
+
+
+
+
+
+
+end
